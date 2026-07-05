4f31a61247289e6a6a6beb25b81d5f4368b7e675
harshisingh777 <harshisingh2609@gmail.com>
2026-07-05 11:53:01 +0530

    runtime.txt added

--- backend/main.py
+++ backend/main.py
@@ -669,4 +669,5 @@ def ingest_data(
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
